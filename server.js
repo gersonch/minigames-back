3d7b6f9a8f752b6e026f457f15097cc87ea22011
gersonch <78560393+gersonch@users.noreply.github.com>
2025-02-05 19:17:27 -0300

    mejorar config

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ import express from "express";
 import wordOfTheDayRoute from "./app/routes/wordOfTheDay.route.js";
 import cors from "cors";
 import dotenv from "dotenv";
-
+dotenv.config();
 // const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(dotenv.config());
+
 app.use(express.json());
 
 const corsOptions = {
-  origin: ["https://minigames-beta.vercel.app/"], // Cambia esta URL a la del frontend que esté permitido
+  origin: ["https://minigames-beta.vercel.app"], // Cambia esta URL a la del frontend que esté permitido
   methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
   credentials: true, // Habilita el intercambio de cookies entre dominios
 };
