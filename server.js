07ea8de0828d6c88b521a16d2cf638f177334e12
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2025-02-05 00:27:56 -0300

    serverless

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(cors(corsOptions));
 
 app.use("/wordOfTheDay", wordOfTheDayRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+export default app;
