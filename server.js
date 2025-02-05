f8d3b103eb9722cf55d5e4513d705c1f9f106040
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2025-02-05 00:46:42 -0300

    cors again

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import wordOfTheDayRoute from "./app/routes/wordOfTheDay.route.js";
 import cors from "cors";
-import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://minigames-beta.vercel.app/"], // Cambia esta URL a la del frontend que esté permitido
+  origin: ["https://minigames-beta.vercel.app/"], // Cambia esta URL a la del frontend que esté permitido
   methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
+  credentials: true, // Habilita el intercambio de cookies entre dominios
 };
 
 app.use(cors(corsOptions));
